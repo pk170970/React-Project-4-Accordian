ecfb6dc2a7d59a14ab7607ed4e7d0e8f9ec63725
Code Sandbox <hello@codesandbox.io>
2022-12-07 09:34:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e882bd46192b91613d940021e5af5800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85317a9163c4bdee1cbf7429d139c91e",
+    "url": "https://pk170970.github.io/csb-jb3nlo/index.html"
+  },
+  {
+    "revision": "9263871d53d638ee07d3",
+    "url": "https://pk170970.github.io/csb-jb3nlo/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "fdd3bb62b4c4389df8a1",
+    "url": "https://pk170970.github.io/csb-jb3nlo/static/js/2.250c9920.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://pk170970.github.io/csb-jb3nlo/static/js/2.250c9920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9263871d53d638ee07d3",
+    "url": "https://pk170970.github.io/csb-jb3nlo/static/js/main.35bbe1dd.chunk.js"
+  },
+  {
+    "revision": "45243c1c64e7693c3c22",
+    "url": "https://pk170970.github.io/csb-jb3nlo/static/js/runtime-main.42e3ff7e.js"
+  }
+]);
